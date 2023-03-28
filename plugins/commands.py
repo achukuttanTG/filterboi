2b4ecdf5e594b2b581529365bd3992f2a7a93e36
achukuttanTG <104365884+achukuttanTG@users.noreply.github.com>
2023-03-28 19:08:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -209,8 +209,8 @@ async def bot_status(client,message):
     )
 
 
-@trojanz.on_message(filters.command('start') filters.group & filters.private)
-async def start(client, message):
+@trojanz.on_message(filters.command('ok') filters.group & filters.private)
+async def ok(client, message):
     await message.reply_text(
         text=Script.START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
